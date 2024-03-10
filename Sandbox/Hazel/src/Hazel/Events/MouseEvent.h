84ff2bece098f8fcf1b7dee50b200234d0f64901
张道强 <zdq@meitu.com>
2024-03-10 17:50:27 +0800

    event system_v1

---
+++ Sandbox/Hazel/src/Hazel/Events/MouseEvent.h
@@ -0,0 +1,13 @@
+#ifndef SANBOX_MOUSEEVENT_H
+
+#define SANBOX_MOUSEEVENT_H
+
+#include "Event.h"
+
+namespace Hazel {
+
+class MouseMovedEvent : public Event {};
+
+} // namespace Hazel
+
+#endif // SANBOX_MOUSEEVENT_H
