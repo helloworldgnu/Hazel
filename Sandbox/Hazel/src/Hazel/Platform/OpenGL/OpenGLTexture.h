07efe2905ba56d097539117bf5d8d454a9109177
helloworldgnu <helloworldgnu@gmail.com>
2024-04-14 23:05:03 +0800

    优化-合并colorShader和TextureShader

--- Sandbox/Hazel/src/Hazel/Platform/OpenGL/OpenGLTexture.h
+++ Sandbox/Hazel/src/Hazel/Platform/OpenGL/OpenGLTexture.h
@@ -2,22 +2,30 @@
 
 #include <string>
 
+#include "glad/glad.h"
+
+#include "OpenGLShader.h"
 #include "Renderer/Texture.h"
 
 namespace Hazel {
     class OpenGLTexture2D : public Texture2D{
     public:
         explicit OpenGLTexture2D(const std::string& path);
+        OpenGLTexture2D(uint32_t width, uint32_t height);
         ~OpenGLTexture2D() override;
 
         uint32_t GetWidth() const override { return m_width;}
         uint32_t GetHeight() const override { return m_height;}
 
+        void SetData(void *data, uint32_t size) override;
         void Bind(uint32_t slot = 0) const override;
 
     private:
         std::string m_Path;
         uint32_t m_width, m_height;
         uint32_t m_RendererID;
+
+        GLint m_InternalFormat = 0;
+        GLenum m_DataFormat = 0;
     };
 }
