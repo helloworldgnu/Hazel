db1085ce73660ac666c67525ea80f45e4eb7ab60
helloworldgnu <helloworldgnu@gmail.com>
2024-04-14 23:07:05 +0800

    删掉无用的代码

--- Sandbox/Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Sandbox/Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -9,7 +9,7 @@ public:
 
     virtual void OnAttach() override;
     virtual void OnDetach() override;
-    virtual void OnImGuiRender() override;
+    // virtual void OnImGuiRender() override;
 
     void Begin();
     void End();
